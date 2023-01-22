41388726bd3c7c6d2d04ddbb6cc28ec0fa08b5f8
Ryan <81808867+ryu878@users.noreply.github.com>
2023-01-22 18:31:20 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+assets_50 = ['APTUSDT','BTCUSDT','ETHUSDT','EOSUSDT','XRPUSDT','BCHUSDT','LTCUSDT','LINKUSDT','ADAUSDT','DOTUSDT','DOGEUSDT','MATICUSDT','ETCUSDT','BNBUSDT','SOLUSDT','AXSUSDT','SANDUSDT','ATOMUSDT','AVAXUSDT','FTMUSDT','NEARUSDT','GALAUSDT','APEUSDT','GMTUSDT','GALUSDT','OPUSDT']
+
+api_key = ''
+api_secret = ''
+
+url = 'wss://stream.bytick.com/realtime_public'
+endpoint = 'https://api.bybit.com'
+
+domain = 'bybit'
